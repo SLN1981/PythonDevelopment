7071395e17a235fc2c42123bb8e449330f0d7dc5
SLN1981 <narasimhan.ronaldinho007@gmail.com>
2025-06-21 19:21:29 +0530

    Data Types till String Formating

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,10 @@
-print("Hello, World!")
+
 # This script prints "Hello, World!" to the console.
+a="Hello, World!"
+print(a)
+print(type(a))
+age = input("Enter your age")
+print("You will be " + str(int(age) + 1) + " next year.")
+
+my_dogs = ['Sammy', 'Frankie']
+print(my_dogs)
---
+++ Strings.py
@@ -0,0 +1,9 @@
+a = "Hell world"
+print(a)
+print(a[0])  # First character
+print(a[:5])  # First five characters
+print(a[6:])  # Characters from index 6 to the end
+print(len(a))  # Length of the string
+print(a.lower())  # Convert to lowercase
+print(a.upper())  # Convert to uppercase
+print(a.replace("Hell", "Hello"))  # Replace substring
